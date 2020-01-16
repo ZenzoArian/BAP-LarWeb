397b5d8b18d87c22673571210cc7da3dae6430d9
ZenzoX <42100836+ZenzoX@users.noreply.github.com>
2020-01-16 11:57:29 +0100

    Database models met Eloquent

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,24 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Company;
 
 class CompanyController extends Controller
 {
     public function list(){
-      // alle bedrijven ophalen en Opslaan
-      $companies = DB::table('companies')->orderBy('name','asc')->get();
-      // $companies = DB::table('companies')->where('country','=','Bahamas')->where('city','=','Middelkerke')->value('name');
-      // $companies = DB::table('companies')->where('country','=','Bahamas')->where('city','=','Middelkerke')->get();
+      // // alle bedrijven ophalen en Opslaan
+      // $companies = DB::table('companies')->orderBy('name','asc')->get();
+      // // $companies = DB::table('companies')->where('country','=','Bahamas')->where('city','=','Middelkerke')->value('name');
+      // // $companies = DB::table('companies')->where('country','=','Bahamas')->where('city','=','Middelkerke')->get();
+      //
+      // return view('company.list', ['bedrijven' => $companies]);
 
-      return view('company.list', ['bedrijven' => $companies]);
+      // -------------------------
+
+      $companies = Company::all();
+      dd($companies);
+
     }
 
     public function details($id){
-      //haal bedrijf uit database waar id=id
-      $company = DB::table('companies')->where('id','=', $id)->first();
-      // DB::table('companies')->find($id);
+      // //haal bedrijf uit database waar id=id
+      // $company = DB::table('companies')->where('id','=', $id)->first();
+      // // DB::table('companies')->find($id);
+      //
+      // return view('company.details',['bedrijf' => $company]);
 
-      return view('company.details',['bedrijf' => $company]);
+      //---------------------------
+
+      $company = Company::find($id);
+      dd($company);
 
     }
 }
