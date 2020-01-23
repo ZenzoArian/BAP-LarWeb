436eed77d7c7b2883597ccd46b9e73e304e39502
ZenzoX <42100836+ZenzoX@users.noreply.github.com>
2020-01-23 16:52:13 +0100

    custome 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,10 @@
-aaaaaaaaaa
+@extends('layouts.app')
+
+
+@section('content')
+
+<p class="P404B">404-Not found</p>
+<p class="P404S">It's not a bug if you call it a feature.</p>
+<p class="P404I">¯\_(ツ)_/¯</p>
+
+@endsection
